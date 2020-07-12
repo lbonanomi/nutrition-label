820e0c77b90f40dc60ed8dbdf5c35f761cecfcbf
ωξβ <lbonanomi@gmail.com>
2020-07-12 14:39:53 -0400

    colorize

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,43 @@
 #!/bin/sh -l
 
+
+[[ -n "$TOKEN" ]] || printf '\e[1;31m%-6s\e[m' "Create a secret called \"TOKEN\" with write permission to $GITHUB_REPOSITORY"
+[[ -n "$TOKEN" ]] || exit 1
+
+
 # Get PR stats
 #
+
+printf '\e[1;32m%-6s\e[m' "Counting collaborating repos"
 export MIX_WITH=$(curl -s -u :$TOKEN "https://api.github.com/search/issues?q=author:$GITHUB_ACTOR+is:pr+is:merged+is:public+-user:$GITHUB_ACTOR" | jq .items[].repository_url | sort | uniq | wc -l) 
 
+printf '\e[1;32m%-6s\e[m' "Counting opened PRs"
 export PULL_COUNT=$(curl -s -u :$TOKEN "https://api.github.com/search/issues?q=author:$GITHUB_ACTOR+is:pr+is:merged+is:public+-user:$GITHUB_ACTOR" | jq .total_count)
 
 # Get issue stats
 #
+printf '\e[1;32m%-6s\e[m' "Counting opened issues"
 export JAM_WITH=$(curl -s -u :$TOKEN "https://api.github.com/search/issues?q=iis:issue+commenter:$GITHUB_ACTOR+-user:$GITHUB_ACTOR" | jq .items[].repository_url | sort | uniq | wc -l)
 
 # Get language stats
 #
 
-# Include forked repos
-#curl -s https://api.github.com/users/$GITHUB_ACTOR/repos | jq .[].languages_url | tr -d '"' | while read dump
+if [[ $1 == "true" ]]
+then
+	# Include forked repos
+	curl -s https://api.github.com/users/$GITHUB_ACTOR/repos | jq .[].languages_url | tr -d '"' | while read dump
+	do
+		curl -s $dump | awk '/:/ { gsub(/\"/,"");gsub(/:/,"");gsub(/,/,""); print; }'
+	done > BUFF
+else
+	# Exclude forked repos
+	curl -s https://api.github.com/users/$GITHUB_ACTOR/repos | jq '.[] | "\(.languages_url ) \(.fork)"' | tr -d '"' | awk '/false$/ { print $1 }' | while read dump
+	do
+		curl -s $dump | awk '/:/ { gsub(/\"/,"");gsub(/:/,"");gsub(/,/,""); print; }' 
+	done > BUFF
+fi
 
-# Exclude forked repos
-curl -s https://api.github.com/users/$GITHUB_ACTOR/repos | jq '.[] | "\(.languages_url ) \(.fork)"' | tr -d '"' | awk '/false$/ { print $1 }' | while read dump
-
-do
-	curl -s $dump | awk '/:/ { gsub(/\"/,"");gsub(/:/,"");gsub(/,/,""); print; }' 
-done > BUFF
-
+printf '\e[1;32m%-6s\e[m' "Tallying lngauges"
 awk '{ print $1 }' BUFF | sort | uniq | while read uniq_lang
 do
 	awk '$1 == "'$uniq_lang'" { a=a+$2 } END { print "'$uniq_lang'",a }'  BUFF >> STATS
@@ -30,10 +45,14 @@ done
 
 # Count local repos
 #
+printf '\e[1;32m%-6s\e[m' "Tallying user's repositories"
 export REPO_COUNT=$(curl -s https://api.github.com/users/$GITHUB_ACTOR/repos | jq .[].full_name | wc -l)
 
 # Calculate language stats
 #
+
+printf '\e[1;32m%-6s\e[m' "Calculating language use"
+
 export LANG1_NAME=$(sort -rnk2 STATS | head -1 | awk '{ print $1 }')
 export LANG1_BYTES=$(sort -rnk2 STATS | head -1 | awk '{ printf "%i KB\n", $2 / 1024 }')
 
@@ -60,7 +79,7 @@ CURRENT_SHA=$(curl -L -s -u :$TOKEN https://api.github.com/repos/$GITHUB_REPOSIT
 
 # Push new label
 #
-curl -s -u :$TOKEN -X PUT -d '{ "message":"Re-label", "sha":"'$CURRENT_SHA'", "content":"'$(cat label.svg | tr -d '\n\r')'"}' https://api.github.com/repos/$GITHUB_REPOSITORY/contents/label.svg
+curl -s -u :$TOKEN -X PUT -d '{ "message":"Re-label", "sha":"'$CURRENT_SHA'", "content":"'$(cat label.svg | tr -d '\n\r')'"}' https://api.github.com/repos/$GITHUB_REPOSITORY/contents/label.svg | jq 
 
 rm STATS
 rm BUFF
